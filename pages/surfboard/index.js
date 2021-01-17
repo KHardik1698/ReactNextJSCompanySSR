6a1479cab44bb34549e6dc8b6982759e83ecf045
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-17 18:50:45 +0530

    Created Surfboard Page
    Added Basic Header & page Title

---
+++ pages/surfboard/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function Surfboard() {
+  return (
+    <div>
+      <Head>
+        <title>Surfboard</title>
+      </Head>
+      <div>
+        <h1>Surfboard</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Surfboard;

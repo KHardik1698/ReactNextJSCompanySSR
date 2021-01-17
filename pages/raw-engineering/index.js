bcbbe85bb594bc09cd58136152f9da17154d37d0
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-17 18:55:32 +0530

    Created Raw Engineering Page
    Added Basic Header & page Title

---
+++ pages/raw-engineering/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function RawEngineering() {
+  return (
+    <div>
+      <Head>
+        <title>Raw Engineering</title>
+      </Head>
+      <div>
+        <h1>Raw Engineering</h1>
+      </div>
+    </div>
+  );
+}
+
+export default RawEngineering;

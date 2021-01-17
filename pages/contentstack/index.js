4e0224c53977a40c4fd47cee028efe678c31fbdd
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-17 18:57:19 +0530

    Created Contentstack Page
    Added Basic Header & page Title

---
+++ pages/contentstack/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function Contentstack() {
+  return (
+    <div>
+      <Head>
+        <title>Contentstack</title>
+      </Head>
+      <div>
+        <h1>Contentstack</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Contentstack;

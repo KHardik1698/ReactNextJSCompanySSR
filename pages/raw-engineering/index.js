fc88859fbc91094cb7b8164037296e09ff310c18
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-18 17:16:44 +0530

    Fetched Raw Engineering Data using Axios
    Raw Engineering Data available from Props

--- pages/raw-engineering/index.js
+++ pages/raw-engineering/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import axios from "axios";
+import companyUrl from "../../constants";
 
-function RawEngineering() {
+function RawEngineering(props) {
+  let rawEngineering = props.rawEngineering[0];
   return (
     <div>
       <Head>
@@ -12,5 +15,14 @@ function RawEngineering() {
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  let { data } = await axios.get(companyUrl + "raw-engineering.json");
+  return {
+    props: {
+      rawEngineering: [...data],
+    },
+  };
+};
 
 export default RawEngineering;

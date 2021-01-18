20c957e918b4d01614274d7a684e9c87b397d909
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-18 19:54:37 +0530

    Displayed Surfboard Data on Surfboard Page
    Currently Surfboard Page has plain markup

--- pages/surfboard/index.js
+++ pages/surfboard/index.js
@@ -21,22 +21,60 @@ function Surfboard(props) {
           <a>Contentstack</a>
         </Link>
       </div>
+      <div>
+        <nav>
+          <img src={surfboard.surfboardLogo} alt="" />
+          <div>
+            {surfboard.navigationLinks.map((link, i) => {
+              return <p key={i}>{link}</p>;
+            })}
+          </div>
+        </nav>
+        <div>
+          <h1>{surfboard.bannerHeading}</h1>
+          <h3>{surfboard.bannerSubHeading}</h3>
+        </div>
+        <div>
+          <img src={surfboard.portfolioImage} alt="" />
+          {surfboard.companies.map((company, i) => {
+            return (
+              <div key={i}>
+                <Link href={company.siteLink}>
+                  <a>{company.companyName}</a>
+                </Link>
+                <p>{company.description}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          {surfboard.team.map((team, i) => {
+            return (
+              <div key={i}>
+                <img src={team.image} alt="" />
+                <p>{team.name}</p>
+                <p>{team.message}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          {surfboard.philosophy.map((philosophy, i) => {
+            return (
+              <div key={i}>
+                <img src={philosophy.image} alt="" />
+                <p>{philosophy.title}</p>
+                <p>{philosophy.content}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  // await axios
-  //   .get(companyUrl + "surfboard.json")
-  //   .then((response) => {
-  //     return response;
-  //   })
-  //   .then((data) => {
-  //     console.log(data.data);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
   let { data } = await axios.get(companyUrl + "surfboard.json");
   return {
     props: {

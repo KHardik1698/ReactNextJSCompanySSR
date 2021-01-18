94ee987361c0a3de4b0ee8eddbe5ccaceb4eebbc
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-18 20:25:46 +0530

    Displayed Raw Engineering Data on Raw Engineering Page
    Currently Raw Engineering Page has plain markup

--- pages/raw-engineering/index.js
+++ pages/raw-engineering/index.js
@@ -12,6 +12,45 @@ function RawEngineering(props) {
       <div>
         <h1>Raw Engineering</h1>
       </div>
+      <div>
+        <p>{rawEngineering.title}</p>
+        <p>{rawEngineering.subtitle}</p>
+        <p>{rawEngineering.shortIntro}</p>
+      </div>
+      <div>
+        {rawEngineering.whatWeDo.map((task, i) => {
+          return (
+            <div key={i}>
+              <img src={task.imageUrl} alt="" />
+              <p>{task.heading}</p>
+              <p>{task.subHeading}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        {rawEngineering.latestWork.map((work, i) => {
+          return (
+            <div key={i}>
+              <img src={work.imageUrl} alt="" />
+              <p>{work.caseStudyHeading}</p>
+              <p>{work.title}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        {rawEngineering.testimonials.map((testimony, i) => {
+          return (
+            <div key={i}>
+              <img src={testimony.imageUrl} alt="" />
+              <p>{testimony.name}</p>
+              <p>{testimony.companyName}</p>
+              <p>{testimony.message}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }

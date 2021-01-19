2bde1a27628406582b80ede313bbe87eded75aed
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-19 20:44:16 +0530

    Added Styling to Home Page
    Added Background Image with Company Navigation Icons

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,44 @@
 import Head from "next/head";
 import Link from "next/link";
+import axios from "axios";
+import companyUrl from "../constants";
+import styles from "../styles/index.module.css";
 
-function Home() {
+function Home(props) {
+  let home = props.home[0];
   return (
     <div>
       <Head>
         <title>Company</title>
         <link rel="icon" href="/logo-white.png" />
       </Head>
-      <div>
-        <h1>Home</h1>
-      </div>
-      <div>
+      <div className={styles["home-container"]}>
         <nav>
-          <Link href="/surfboard">
-            <a>Surfboard</a>
-          </Link>
-          <Link href="/raw-engineering">
-            <a>Raw Engineering</a>
-          </Link>
-          <Link href="/contentstack">
-            <a>Contentstack</a>
-          </Link>
+          <div className={styles["logo-container"]}>
+            {home.companies.map((company, i) => {
+              return (
+                <div className={styles["company"]} key={i}>
+                  <Link href={company.url}>
+                    <img className={styles["logo-image"]} src={company.logo} alt="Logo" />
+                  </Link>
+                  <p>{company.companyName}</p>
+                </div>
+              );
+            })}
+          </div>
         </nav>
       </div>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  let { data } = await axios.get(companyUrl + "home.json");
+  return {
+    props: {
+      home: [...data],
+    },
+  };
+};
 
 export default Home;

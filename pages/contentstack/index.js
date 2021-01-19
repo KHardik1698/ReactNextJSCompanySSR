82981adf462f9850d821893424e4acf1d62773e4
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-19 11:25:02 +0530

    Displayed Contentstack Data on Contentstack Page
    Currently Contentstack Page has plain markup

--- pages/contentstack/index.js
+++ pages/contentstack/index.js
@@ -12,6 +12,43 @@ function Contentstack(props) {
       <div>
         <h1>Contentstack</h1>
       </div>
+      <nav>
+        <img src={contentstack.contentstackLogo} alt="" />
+        <div>
+          {contentstack.navigationLinks.map((link, i) => {
+            return <p key={i}>{link}</p>;
+          })}
+        </div>
+      </nav>
+      {/* banner */}
+      <div>
+        <h1>{contentstack.heading}</h1>
+        <h3>{contentstack.subHeading}</h3>
+        <button>TRY FOR FREE</button>
+        <p>Request a Demo</p>
+      </div>
+      <div>
+        <h1>Why Choose Contentstack?</h1>
+        {contentstack.whyChooseContentstack.map((whyChoose, i) => {
+          return (
+            <div key={i}>
+              <h3>{whyChoose.title}</h3>
+              <p>{whyChoose.content}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <h1>{contentstack.bottomBanner.title}</h1>
+        <button>REQUEST A DEMO</button>
+        <button>TRY FOR FREE</button>
+      </div>
+      <hr />
+      <div>
+        {contentstack.footer.map((footerLinks, i) => {
+          return <li key={i}>{footerLinks}</li>;
+        })}
+      </div>
     </div>
   );
 }
